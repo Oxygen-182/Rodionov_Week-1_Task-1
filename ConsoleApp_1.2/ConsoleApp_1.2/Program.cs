aecd6a5c26da645d7bf4953c4c644e23250ae8b5
Oxygen-182 <119591922+Oxygen-182@users.noreply.github.com>
2022-12-02 17:57:21 +0300

    Add files via upload

---
+++ ConsoleApp_1.2/ConsoleApp_1.2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Введите сторону a:");
+                    a = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Введите сторону b:");
+                    b = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Введите сторону c:");
+                    c = double.Parse(Console.ReadLine());
+
+                    if (a == 0 || b == 0 || c == 0) throw new Exception("Сторона не может иметь значение равное нулю");
+                    if (a <= 0 || b <= 0 || c <= 0) throw new Exception("Сторона не может иметь отрицательное значение");
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"{ex.Message}\n\n");
+                }
+            }
+
+            if (a == b && b == c && a == c)
+            {
+                Console.WriteLine("Треугольник - равносторонний");
+                return;
+            }
+
+            if (a == b || b == c || a == c)
+            {
+                Console.WriteLine("Треугольник - равнобедренный");
+            }
+            else
+            {
+                Console.WriteLine("Треугольник - НЕ равнобедренный");
+            }
+        }
+    }
+}
